672fd11c0d3ff15ae957b4850293401bbdee586b
Andrew <andrew.smith5661@gmail.com>
2018-03-22 15:30:49 +0200

    Added empty chart for VMs migration count

--- src/main/java/Form1.java
+++ src/main/java/Form1.java
@@ -20,11 +20,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.List;
 
 public class Form1 {
     private JPanel mainPanel;
-    private JPanel panelSlaViolationTimeChart;
-    private JPanel panelPowerConsumptionChart;
+    private JPanel slaPanel;
+    private JPanel powerPanel;
+    private JPanel migrationPanel;
 
     private JButton selectConfigButton;
     private JButton runButton;
@@ -34,24 +36,28 @@ public class Form1 {
 
     private File selectedFile = null;
 
-    private XYPlot plotSlaViolationTime;
-    private int datasetSlaIndex = 0;
+    private XYPlot slaPlot;
+    private XYPlot powerPlot;
+    private XYPlot migrationPlot;
 
-    private XYPlot plotPowerConsumption;
+    private int datasetSlaIndex = 0;
     private int datasetPowerIndex = 0;
+    private int datasetMigrationIndex = 0;
 
     public Form1() {
         JFrame frame = new JFrame("Diss");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setContentPane(mainPanel);
-        frame.setSize(1250, 750);
+        frame.setSize(1250, 1000);
         frame.setLocationRelativeTo(null);
 
         mainPanel.setLayout(null);
-        panelSlaViolationTimeChart.setBounds(170,10,1050,330);
-        panelSlaViolationTimeChart.setLayout(new BorderLayout());
-        panelPowerConsumptionChart.setBounds(170,355,1050,330);
-        panelPowerConsumptionChart.setLayout(new BorderLayout());
+        slaPanel.setBounds(170,10,1050,290);
+        slaPanel.setLayout(new BorderLayout());
+        powerPanel.setBounds(170,320,1050,290);
+        powerPanel.setLayout(new BorderLayout());
+        migrationPanel.setBounds(170,630,1050,290);
+        migrationPanel.setLayout(new BorderLayout());
 
         selectConfigButton.setBounds(10,10,150,30);
         runButton.setBounds(10,70,150,30);
@@ -60,45 +66,39 @@ public class Form1 {
         fileNameLabel.setBounds(10,40,300, 30);
         processingLabel.setBounds(10,100,300, 30);
 
-        final JFreeChart slaViolationTimeChart = ChartFactory.createXYLineChart("SLA violation time","Time", "SLA Violation Time",
+        final JFreeChart slaChart = ChartFactory.createXYLineChart("SLA violation time","Time", "SLA Violation Time",
                 null, PlotOrientation.VERTICAL, true, true, false);
-
-        slaViolationTimeChart.setBackgroundPaint(Color.white);
-        plotSlaViolationTime = slaViolationTimeChart.getXYPlot();
-        plotSlaViolationTime.setBackgroundPaint(Color.lightGray);
-        plotSlaViolationTime.setDomainGridlinePaint(Color.white);
-        plotSlaViolationTime.setRangeGridlinePaint(Color.white);
-
-        final ValueAxis axis1 = plotSlaViolationTime.getDomainAxis();
+        slaPlot = slaChart.getXYPlot();
+        final ValueAxis axis1 = slaPlot.getDomainAxis();
         axis1.setAutoRange(true);
-
         final NumberAxis rangeAxis1 = new NumberAxis("Range Axis 1");
         rangeAxis1.setAutoRangeIncludesZero(false);
+        final ChartPanel slaChartPanel = new ChartPanel(slaChart);
+        slaPanel.add(slaChartPanel);
+        slaChartPanel.setDomainZoomable(true);
 
-        final ChartPanel chartPanelSla = new ChartPanel(slaViolationTimeChart);
-        panelSlaViolationTimeChart.add(chartPanelSla);
-        chartPanelSla.setDomainZoomable(true);
-
-        final JFreeChart powerConsumptionChart = ChartFactory.createXYLineChart("Power consumption","Time", "Power consumption",
+        final JFreeChart powerChart = ChartFactory.createXYLineChart("Power consumption","Time", "Power consumption",
                 null, PlotOrientation.VERTICAL, true, true, false);
-
-        powerConsumptionChart.setBackgroundPaint(Color.white);
-        plotPowerConsumption = powerConsumptionChart.getXYPlot();
-        plotPowerConsumption.setBackgroundPaint(Color.lightGray);
-        plotPowerConsumption.setDomainGridlinePaint(Color.white);
-        plotPowerConsumption.setRangeGridlinePaint(Color.white);
-
-        final ValueAxis axis2 = plotPowerConsumption.getDomainAxis();
+        powerPlot = powerChart.getXYPlot();
+        final ValueAxis axis2 = powerPlot.getDomainAxis();
         axis2.setAutoRange(true);
-
         final NumberAxis rangeAxis2 = new NumberAxis("Range Axis 2");
         rangeAxis2.setAutoRangeIncludesZero(false);
+        final ChartPanel powerChartPanel = new ChartPanel(powerChart);
+        powerPanel.add(powerChartPanel);
+        powerChartPanel.setDomainZoomable(true);
 
-        final ChartPanel chartPanelPower = new ChartPanel(powerConsumptionChart);
-        panelPowerConsumptionChart.add(chartPanelPower);
-        chartPanelPower.setDomainZoomable(true);
+        final JFreeChart migrationChart = ChartFactory.createXYLineChart("Migration count","Time", "Migration count",
+                null, PlotOrientation.VERTICAL, true, true, false);
+        migrationPlot = migrationChart.getXYPlot();
+        final ValueAxis axis3 = migrationPlot.getDomainAxis();
+        axis3.setAutoRange(true);
+        final NumberAxis rangeAxis3 = new NumberAxis("Range Axis 3");
+        rangeAxis3.setAutoRangeIncludesZero(false);
+        final ChartPanel migrationChartPanel = new ChartPanel(migrationChart);
+        migrationPanel.add(migrationChartPanel);
+        migrationChartPanel.setDomainZoomable(true);
 
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
 
         selectConfigButton.addActionListener(new ActionListener() {
@@ -125,14 +125,14 @@ public class Form1 {
                 ParseConfig.getData(selectedFile.getAbsolutePath());
                 new Runner(ParseConfig.inputFolder, ParseConfig.outputFolder, ParseConfig.experimentName);
                 processingLabel.setText("Done!");
-                //panelSlaViolationTimeChart.add(getChart(), BorderLayout.NORTH);
+
                 datasetSlaIndex++;
-                plotSlaViolationTime.setDataset(datasetSlaIndex, createDatasetSlaViolationTime());
-                plotSlaViolationTime.setRenderer(datasetSlaIndex, new StandardXYItemRenderer());
+                slaPlot.setDataset(datasetSlaIndex, createDataset(HostPowerModeSelectionPolicyAgent.getTimeList(), HostPowerModeSelectionPolicyAgent.getSlaViolationTimeList(), "Q-learning agent"));
+                slaPlot.setRenderer(datasetSlaIndex, new StandardXYItemRenderer());
 
                 datasetPowerIndex++;
-                plotPowerConsumption.setDataset(datasetPowerIndex, createDatasetPowerConsumption());
-                plotPowerConsumption.setRenderer(datasetPowerIndex, new StandardXYItemRenderer());
+                powerPlot.setDataset(datasetPowerIndex, createDataset(HostPowerModeSelectionPolicyAgent.getTimeList(), HostPowerModeSelectionPolicyAgent.getPowerConsumptionList(), "Q-learning agent"));
+                powerPlot.setRenderer(datasetPowerIndex, new StandardXYItemRenderer());
             }
         });
 
@@ -147,27 +147,13 @@ public class Form1 {
         });
     }
 
-    private XYDataset createDatasetSlaViolationTime() {
-        final XYSeries agent = new XYSeries("Q-learning agent");
-        java.util.List<Double> timeList = HostPowerModeSelectionPolicyAgent.getTimeList();
-        java.util.List<Double> slaViolationTimeList = HostPowerModeSelectionPolicyAgent.getSlaViolationTimeList();
-        for (int i = 0; i < slaViolationTimeList.size(); i++) {
-            agent.add(timeList.get(i),slaViolationTimeList.get(i));
+    private XYDataset createDataset(List<Double> timeList, List<Double> list, String key) {
+        final XYSeries series = new XYSeries(key);
+        for (int i = 0; i < timeList.size(); i++) {
+            series.add(timeList.get(i), list.get(i));
         }
         final XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(agent);
-        return dataset;
-    }
-
-    private XYDataset createDatasetPowerConsumption() {
-        final XYSeries agent = new XYSeries("Q-learning agent");
-        java.util.List<Double> timeList = HostPowerModeSelectionPolicyAgent.getTimeList();
-        java.util.List<Double> powerConsumptionList = HostPowerModeSelectionPolicyAgent.getPowerConsumptionList();
-        for (int i = 0; i < powerConsumptionList.size(); i++) {
-            agent.add(timeList.get(i),powerConsumptionList.get(i));
-        }
-        final XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(agent);
+        dataset.addSeries(series);
         return dataset;
     }
 
